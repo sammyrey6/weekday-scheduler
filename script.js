31ea7d79558aa5d45ab734a7eadc087f17201c17
sammyrey6 <121529677+sammyrey6@users.noreply.github.com>
2023-03-25 20:41:06 -0700

    <clickable-button>

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+// TODO create functioning save button
+const saveButton = document.getElementById('save-btn')
+
+$(document).click(save-btn);
+
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
